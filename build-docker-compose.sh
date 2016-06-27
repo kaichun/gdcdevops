f6e31c854cd38900253843c24864dde35bd2b6c7
kaichun <50533292@qq.com>
2016-06-27 23:49:29 +0800

    delete comme

--- build-docker-compose.sh
+++ build-docker-compose.sh
@@ -1,14 +1,3 @@
-﻿######################################################################################################
-## HPE Software Services GDC China
-## Run the script to compose docker-compose configurations.
-## Steps
-## mkdir /tmp/gdcdevops
-## cd /tmp/gdcdevops
-## curl -k -L https://raw.githubusercontent.com/kaichun/gdcdevops/master/build-docker-compose.sh | bash /dev/stdin
-## To stop: docker-compose stop
-## To start: docker-compose up --no-recreate -d
-######################################################################################################
-
-#!/bin/bash
+﻿#!/bin/bash
 wget https://raw.githubusercontent.com/kaichun/gdcdevops/master/docker-compose.yml --no-check-certificate
 docker-compose up -d
