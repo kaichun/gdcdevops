3eee876c54912ec4211eb2f598166357691010bf
kaichun <50533292@qq.com>
2016-06-27 22:52:47 +0800

    Update build-docker-compose.sh

--- build-docker-compose.sh
+++ build-docker-compose.sh
@@ -1,3 +1,14 @@
+######################################################################################################
+# HPE Software Services GDC China
+# Run the script to compose docker-compose configurations.
+# Steps,
+# mkdir /tmp/gdcdevops
+# cd /tmp/gdcdevops
+# curl -k -L https://github.com/kaichun/gdcdevops/blob/master/build-docker-compose.sh
+# To stop: docker-compose stop
+# To start: docker-compose up --no-recreate -d
+######################################################################################################
+
 #!/bin/bash
 wget https://github.com/kaichun/gdcdevops/blob/master/docker-compose.yml --no-check-certificate
 docker-compose up -d
