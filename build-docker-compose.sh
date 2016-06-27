8c5106d61be67abc8d9855e5db236a5f33acbd12
kaichun <50533292@qq.com>
2016-06-27 22:53:28 +0800

    Update build-docker-compose.sh

--- build-docker-compose.sh
+++ build-docker-compose.sh
@@ -4,7 +4,7 @@
 # Steps,
 # mkdir /tmp/gdcdevops
 # cd /tmp/gdcdevops
-# curl -k -L https://github.com/kaichun/gdcdevops/blob/master/build-docker-compose.sh
+# curl -k -L https://github.com/kaichun/gdcdevops/blob/master/build-docker-compose.sh | bash /dev/stdin
 # To stop: docker-compose stop
 # To start: docker-compose up --no-recreate -d
 ######################################################################################################
